10dbb15be2c31971d75b965ee4f13119303e8b91
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2024-04-08 07:35:03 +0700

    add style to item

--- src/youToBeData.js
+++ src/youToBeData.js
@@ -15,5 +15,13 @@ export const YouToBeDaTa = [
         image : 'https://scr.vn/wp-content/uploads/2020/07/%E1%BA%A2nh-T%E1%BA%BFt-sum-v%E1%BA%A7y.jpg',
         avatar : 'https://antimatter.vn/wp-content/uploads/2022/06/anh-bau-troi-va-hoa.jpg'
 
+    },
+    {
+        id: 3,
+        title : 'This is example of title 3',
+        author : 'This is example of author name 3',
+        image : 'https://scr.vn/wp-content/uploads/2020/07/%E1%BA%A2nh-T%E1%BA%BFt-sum-v%E1%BA%A7y.jpg',
+        avatar : 'https://antimatter.vn/wp-content/uploads/2022/06/anh-bau-troi-va-hoa.jpg'
+
     }
 ]
